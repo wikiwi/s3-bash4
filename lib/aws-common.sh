44254cbae2e1294cb632e6e18dfcfc8ad1551617
Chi Vinh Le <cvl@winged.kiwi>
2015-07-02 08:12:01 +0200

    more comments and bug fixes

--- lib/aws-common.sh
+++ lib/aws-common.sh
@@ -3,58 +3,73 @@
 # Common functions for s3 commands
 
 ##
-# Convert string to hex with max line size of 256
-# $1 string to convert
+# Write error to stderr
+# Arguments:
+#   $1 string to output
 ##
-HEX256() {
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
+
+##
+# Convert string to hex with max line size of 256
+# Arguments:
+#   $1 string to convert
+# Returns:
+#   hex string
+##
+hex256() {
   printf "$1" | xxd -p -c 256
 }
 
 ##
 # Convert string to sha256 hash
-# $1 string to convert
+# Arguments:
+#   $1 string to convert
+# Returns:
+#   hash string
 ##
-SHA256Hash() {
+sha256hash() {
   local output=$(printf "$1" | shasum -a 256)
   echo "${output%% *}"
 }
 
 ##
 # Generate HMAC signature using SHA256
-# $1 signing key in hex
-# $2 string data to sign
+# Arguments:
+#   $1 signing key in hex
+#   $2 string data to sign
+# Returns:
+#   signature
 ##
-HMAC-SHA256() {
-  HEX256 `printf "$2" | openssl dgst -binary -sha256 -mac HMAC -macopt hexkey:$1`
-}
-
-##
-# Generate HMAC signature using SHA256 and output as HEX
-# $1 signing key in hex
-# $2 string data to sign
-##
-HMAC-SHA256-HEX() {
+hmac_sha256() {
   printf "$2" | openssl dgst -binary -hex -sha256 -mac HMAC -macopt hexkey:$1 | sed 's/^.* //'
 }
 
 ##
 # Sign data using AWS Signature Version 4
-# $1 AWS Secret Access Key
-# $2 yyyymmdd
-# $3 AWS Region
-# $4 AWS Service
-# $5 string data to sign
+# Arguments:
+#   $1 AWS Secret Access Key
+#   $2 yyyymmdd
+#   $3 AWS Region
+#   $4 AWS Service
+#   $5 string data to sign
+# Returns:
+#   signature
 ##
-Sign() {
-  local kSigning=$(HMAC-SHA256 $(HMAC-SHA256 $(HMAC-SHA256 $(HMAC-SHA256 $(HEX256 "AWS4$1") $2) $3) $4) "aws4_request")
-  HMAC-SHA256-HEX ${kSigning} $5
+sign() {
+  local kSigning=$(hmac_sha256 $(hmac_sha256 $(hmac_sha256 $(hmac_sha256 $(hex256 "AWS4$1") $2) $3) $4) "aws4_request")
+  hmac_sha256 "${kSigning}" "$5"
 }
 
 ##
 # Get endpoint of specified region
-# $1 region
+# Arguments:
+#   $1 region
+# Returns:
+#   amazon andpoint
 ##
-S3RegionToEndpoint() {
+convS3RegionToEndpoint() {
   case "$1" in
     us-east-1) echo "s3.amazonaws.com"
       ;;
